850ca231980bc7aa7011859aeadacca28a42b07b
johannadevos <johannadevos@gmail.com>
2018-02-27 15:45:37 +0100

    All data can be read and preprocessed correctly, except for STAT_C_NL. The problem with STAT_C_NL has to do with the way the data were transcribed, and not with the code. The data file needs to be corrected before the desired preprocessing will be possible.

--- preprocessing.py
+++ preprocessing.py
@@ -23,7 +23,9 @@ random.seed(2017)
 nltk.data.path.append("U:/nltk_data")
 
 # Set working directory
-os.chdir("C:/Users/U908153/Desktop/GitHub/StudyLanguage/data")
+path = "C:/Users/U908153/Desktop/GitHub/StudyLanguage/data"
+os.chdir(path)
+
 
 ### ------------------
 ### PREPARING THE DATA
@@ -45,13 +47,38 @@ def preprocess(raw_text):
     
     # For empty anwers, insert a dash
     text = raw_text.replace("Antwoord:\n", "Antwoord: -\n")
+    
+    # Preprocessing for STAT_C, which has a different format than the other transcriptions
+    text = text.replace("\n2)", " 2)") # Put the 3-part answer on one line
+    text = text.replace("\n3)", " 3)") # Idem
+    text = re.sub(r'-{2,}', "", text) # Remove two or more dashes
+    
+    # Preprocessing for STAT_C_EN
+    text = re.sub(r'Answer: *\nDecision', "Answer: Decision", text) # Remove newline between Answer and Decision
+    text = re.sub(r'Answer: *\nCausal', "Answer: Causal", text) # Remove newline between Answer and Causal explanation
+    text = re.sub(r'Subtracted [p|P]oints 2a \(original\): -?\d*.?', "", text) # Remove the original points
+    text = re.sub(r'Subtracted [p|P]oints \(Rick\): -?\d*.?', "", text) # Remove the points assigned by Rick
+    text = text.replace("Exam number", "Tentamennummer")
+    text = text.replace("Subject code", "Subjectcode")
+    text = text.replace("Points", "Punten")
+    text = text.replace("Answer", "Antwoord")
+    
+    # Preprocessing for STAT_C_NL
+    text = text.replace("Punten 4a", "Punten")
+    text = re.sub(r'Antwoord: *\nBeslissing', "Antwoord: Beslissing", text) # Remove newline between Answer and Decision
+    text = re.sub(r'Antwoord: *\nCausale', "Antwoord: Causale", text) # Remove newline between Answer and Causal explanation
+    text = re.sub(r'Aftrekpunten 2a: -?\d*.?', "", text) # Remove the original points
         
     # Remove white space
     text = text.replace("\\n", "")
     text = text.replace(" /n ", "")
     text = text.replace("/n", "")
+    text = text.replace("\n\n", "\n")
+    text = text.replace("\n\n", "\n")
+    text = text.replace("\n\n", "\n")
     text = text.replace("  ", " ")
     text = text.replace("\n\nTentamennummer", "\nTentamennummer")
+    text = text.replace("Punten:  ", "Punten: ")
      
     # Replace curly quotes
     text = text.replace(chr(0x2019), chr(0x0027)) # Replace right single curly quote with straight quote
@@ -63,102 +90,155 @@ def preprocess(raw_text):
     text = text.replace("can't", "cannot")
     text = text.replace("n't", " not")
     
+    # Other
+    text = text.replace("???", "?")
+    
     return text
 
 
 # Rearrange the students' answers in a dataframe
-def create_df(text):
+def create_df(text, filename):
     print("Creating data frame for the student answers...")
     
+    # Split text by newline
+    text = text.split("\n")
+
+    # Create empty lists
     exam_numbers = []
     subject_codes = []
     grades = []
     answers = []
-    
-    # Split text by newline
-    text = text.split("\n")
-    
+        
+    # Extract info    
+    if not "STAT_C" in filename:
+        mod = 4
+    elif "STAT_C" in filename:
+        mod = 8
+        grades_2a_dec = []
+        answers_2a_dec = []
+        grades_2a_caus = []
+        answers_2a_caus = []
+
     # Extract information from running text
     for i in range(len(text)):
-        if i%4 == 0:
+        if i%mod == 0:
             exam_number = text[i][16:]
+            #print(exam_number)
             exam_numbers.append(exam_number)
-        elif i%4 == 1:
+        elif i%mod == 1:
             subject_code = text[i][13:]
+            #print(subject_code)
             subject_codes.append(subject_code)
-        elif i%4 == 2:
-            grade = text[i][7:]
+        elif i%mod == 2:
+            grade = text[i][8:]
+            #print(grade)
             grades.append(int(grade))
-        elif i%4 == 3:
+            #grades.append(grade)
+        elif i%mod == 3:
             answer = text[i][10:]
+            #print(answer)
             answers.append(answer)
-            
+        
+        if "STAT_C" in filename:
+            if i%mod == 4:
+                grade_2a_dec = text[i][8:]
+                grades_2a_dec.append(int(grade_2a_dec))
+                #grades_2a_dec.append(grade_2a_dec)
+            elif i%mod == 5:
+                answer_2a_dec = text[i][10:]
+                answers_2a_dec.append(answer_2a_dec)                
+            elif i%mod == 6:
+                grade_2a_caus = text[i][8:]
+                grades_2a_caus.append(int(grade_2a_caus))
+                #grades_2a_caus.append(grade_2a_caus)
+            elif i%mod == 7:
+                answer_2a_caus = text[i][10:]
+                answers_2a_caus.append(answer_2a_caus)
+                                                    
     # Create dataframe
-    df = pd.DataFrame({'ExamNumber': exam_numbers, 'SubjectCode': subject_codes, 'Grade': grades, 'Answer': answers})      
+    if not "STAT_C" in filename:
+        df = pd.DataFrame({'ExamNumber': exam_numbers, 'SubjectCode': subject_codes, 'Grade': grades, 'Answer': answers})      
     
-    # Add empty columns that can later contain tokenized and lemmatized data
-    df['Tokenized'] = ""
-    df['Lemmatized'] = ""
-    df['NoStops'] = "" 
+        # Add empty columns that can later contain tokenized and lemmatized data
+        df['Tokenized'] = ""
+        df['Lemmatized'] = ""
+        df['NoStops'] = ""     
+        
+        # Change order of columns
+        cols = ['SubjectCode', 'ExamNumber', 'Grade', 'Answer', 'Tokenized', 'Lemmatized', 'NoStops']    
+        df = df[cols]
+        
+    elif "STAT_C" in filename:
+        df = pd.DataFrame({'ExamNumber': exam_numbers, 'SubjectCode': subject_codes, 'Grade4a': grades, 'Answer4a': answers, 'Grade2aDec': grades_2a_dec, 'Answer2aDec': answers_2a_dec, 'Grade2aCaus': grades_2a_caus, 'Answer2aCaus': answers_2a_caus}) 
     
-    # Change order of columns
-    cols = ['SubjectCode', 'ExamNumber', 'Grade', 'Answer', 'Tokenized', 'Lemmatized', 'NoStops']
-    df = df[cols]    
-
+        # Add empty columns that can later contain tokenized and lemmatized data
+        df['Tokenized4a'] = ""
+        df['Lemmatized4a'] = ""
+        df['NoStops4a'] = ""  
+        df['Tokenized2aDec'] = ""
+        df['Lemmatized2aDec'] = ""
+        df['NoStops2aDec'] = ""            
+        df['Tokenized2aCaus'] = ""
+        df['Lemmatized2aCaus'] = ""
+        df['NoStops2aCaus'] = ""           
+          
+        cols = ['SubjectCode', 'ExamNumber', 'Grade4a', 'Answer4a', 'Tokenized4a', 'Lemmatized4a', 'NoStops4a', 'Grade2aDec', 'Answer2aDec', 'Tokenized2aDec', 'Lemmatized2aDec', 'NoStops2aDec', 'Grade2aCaus', 'Answer2aCaus', 'Tokenized2aCaus', 'Lemmatized2aCaus', 'NoStops2aCaus']
+        df = df[cols]
+    
     return df, cols
 
 
 # Tokenize, lemmatize, and remove stop words
-def tok_lem(df):
+def tok_lem(df, filename):
     print("Tokenizing, lemmatizing, and removing stop words...")
-    
-    # Make SubjectCode the index of the dataframe
-    #df = df.set_index("SubjectCode") --> with this line, preprocessing the book no longer works
     
     # Set up tokenizer and lemmatizer
     tokenizer = RegexpTokenizer(r'\w+')
     lemmatizer = WordNetLemmatizer()    
     
-    for i in range(len(df)):
-        answer = df['Answer'][i]
+    if not "STAT_C_EN" in filename:
+        answers = ['Answer']
+    elif "STAT_C_EN" in filename:
+        answers = ['Answer4a', 'Answer2aDec', 'Answer2aCaus']
         
-        # Preprocess     
-        answer = answer.replace("'s", "") # Remove possessive 's
-        answer = answer.lower() # Make lowercase
-        answer = re.sub("\d+", "", answer) # Remove numbers
- 
-        # Preprocess when using spelling-corrected data
-        answer = answer.replace("_abbreviation", "")
-        answer = answer.replace("_nonexistent", "")
-        answer = answer.replace("_dutch", "")
-        answer = answer.replace("_german", "")
-        
-        # Tokenize
-        tok_answer = tokenizer.tokenize(answer) # also removes apostrophe
-                                       
-        df.at[i, 'Tokenized'] = tok_answer                             
-        #df['Tokenized'][i] = tok_answer
-        
-        # Lemmatize
-        lem_answer = []
-        
-        for word in tok_answer:
-            lemma = lemmatizer.lemmatize(word, pos = 'v') # default POS is 'n'
+    for question in answers:
+        for i in range(len(df)):
+            answer = df[question][i]
             
-            # Hand-crafted rules for words not handled well by lemmatizer
-            if lemma.startswith("whorf"):
-                lemma = "whorf"
+            # Preprocess     
+            answer = answer.replace("'s", "") # Remove possessive 's
+            answer = answer.lower() # Make lowercase
+            answer = re.sub("\d+", "", answer) # Remove numbers
+     
+            # Preprocess when using spelling-corrected data
+            answer = answer.replace("_abbreviation", "")
+            answer = answer.replace("_nonexistent", "")
+            answer = answer.replace("_dutch", "")
+            answer = answer.replace("_german", "")
             
-            lem_answer.append(lemma)
-        
-        df.at[i, 'Lemmatized'] = lem_answer
-        #df['Lemmatized'][i] = lem_answer
-
-        # Remove stop words
-        stopped_lemmas = [i for i in lem_answer if not i in stopwords.words('english')]
-        df.at[i, 'NoStops'] = stopped_lemmas
-        #df['NoStops'][i] = stopped_lemmas
-        
+            # Tokenize
+            tok_answer = tokenizer.tokenize(answer) # also removes apostrophe
+                                           
+            df.at[i, 'Tokenized%s' % question[6:]] = tok_answer                             
+            
+            # Lemmatize
+            lem_answer = []
+            
+            for word in tok_answer:
+                lemma = lemmatizer.lemmatize(word, pos = 'v') # default POS is 'n'
+                # TO DO: Look into the lemmatizer. When pos is set to 'v', nouns, are not lemmatised, for example: 'situations' remains unchanged.
+                
+                # Hand-crafted rules for words not handled well by lemmatizer
+                if lemma.startswith("whorf"):
+                    lemma = "whorf"
+                
+                lem_answer.append(lemma)
+            
+            df.at[i, 'Lemmatized%s' % question[6:]] = lem_answer
+            
+            # Remove stop words
+            stopped_lemmas = [i for i in lem_answer if not i in stopwords.words('english')]
+            df.at[i, 'NoStops%s' % question[6:]] = stopped_lemmas
         
     return df
 
@@ -175,12 +255,14 @@ def dictionary(df):
     return dictionary
 
 
+### --------
+### SAVING RESULTS
+### --------
+
 # Set up output file
 def setup_outfile():
-    #outfile = "C:/Users/johan/Dropbox/PhD/Courses/Text and multimedia mining/Project/Results.txt"
-    #outfile = "D:/Dropbox2/Dropbox/PhD/Courses/Text and multimedia mining/Project/Results2.txt"
-    outfile = "D:/Dropbox2/Dropbox/PhD/Courses/Text and multimedia mining/Project/Python"
-    headers = ["Model", "Counting", "TrainOrTest", "TrainingData", "Mapping", "SpellingCorrection", "Pearson", "SigPearson", "Spearman", "SigSpearman"]
+    outfile = path
+    headers = ["Header1", "Header2"]
     headers2 = "\t".join(headers)
     f = open(outfile, 'w') 
     f.write(headers2)
@@ -205,9 +287,28 @@ def save_to_file(info, outfile):
 if __name__ == "__main__":
     
     # Read and prepare student data
-    raw_data = open_file('AIP_A_EN_uncorrected.txt') # Read student answers
+    #filename = 'AIP_A_EN_uncorrected.txt'
+    #filename = 'AIP_A_EN_corrected.txt'
+    #filename = 'AIP_A_NL_uncorrected.txt'
+    #filename = 'AIP_A_NL_corrected.txt'  
+    #filename = 'STAT_A_EN_uncorrected.txt'
+    #filename = 'STAT_A_EN_corrected.txt'             
+    #filename = 'STAT_A_NL_uncorrected.txt'
+    #filename = 'STAT_A_NL_corrected.txt'
+    #filename = 'STAT_C_EN_uncorrected.txt'
+    filename = 'STAT_C_EN_corrected.txt'
+
+    # TO DO. First, assign grades to the two subquestions of 2a          
+    #filename = 'STAT_C_NL_uncorrected.txt' 
+    #filename = 'STAT_C_NL_corrected.txt'
+    
+    raw_data = open_file(filename)
     prep_data = preprocess(raw_data) # Preprocess student answers
-    df, cols = create_df(prep_data) # Create dataframe of student answers
-    #df = df[:10]
-    df = tok_lem(df) # Tokenize and lemmatize all answers, and remove stop words
-    dictio = dictionary(df) # Create dictionary of vocabulary
+    df, cols = create_df(prep_data, filename) # Create dataframe of student answers
+    
+    #pd.set_option('display.max_rows', None) # Print the whole pandas table, don't truncate
+    #print(df)
+    
+    df = df[:10]
+    df = tok_lem(df, filename) # Tokenize and lemmatize all answers, and remove stop words
+    #dictio = dictionary(df) # Create dictionary of vocabulary --> currently, doesn't work for STAT_C_EN
