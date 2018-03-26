3571f981215b0cad2fbd0b42293a18a2c20564f9
Johanna de Vos <johannadevos@gmail.com>
2018-03-26 13:55:18 +0200

    Squashed commit of the following:
    commit 62412db6c242297587ded1e9b1cc74d5759cebab
    Author: Johanna de Vos <johannadevos@gmail.com>
    Date:   Mon Mar 26 13:53:27 2018 +0200
    
        analysing.py now creates a joined dataframe containing both the LCA results and the subject info, so that the results can be analysed by subject filters.
    
    commit a7e4cda71503d3d3c3e570a368f02d7b8ecc3541
    Author: Johanna de Vos <johannadevos@gmail.com>
    Date:   Mon Mar 26 11:15:26 2018 +0200
    
        Added optional arguments for truncation and nationality.
    
    commit 9778483146f2bb996e0007bc3e60235423b5ade9
    Author: Johanna de Vos <johannadevos@gmail.com>
    Date:   Mon Mar 26 11:06:43 2018 +0200
    
        Make SubjectCode column the index of the subject_info dataframe.
    
    commit e665836d01d65a8d47b94f823be75917f86bbcf2
    Author: Johanna de Vos <johannadevos@gmail.com>
    Date:   Mon Mar 26 11:00:54 2018 +0200
    
        Read in subject info in analysing.py.
    
    commit e18ba3f5145d097dfc05a2d5bf07546c9512001d
    Author: Johanna de Vos <johannadevos@gmail.com>
    Date:   Mon Mar 26 10:57:34 2018 +0200
    
        LCA results files contain subject code instead of filename. Minor changes to the structure of preprocessing.py.

--- src/analysing.py
+++ src/analysing.py
@@ -6,6 +6,8 @@ import os
 
 import pandas as pd
 
+import preprocessing as prep
+
 
 def create_pandas_df(lca_results_dir, filename):
     """A function that reads the LCA results (per exam) from a CSV file into
@@ -13,14 +15,7 @@ def create_pandas_df(lca_results_dir, filename):
     
     # NB: Make sure that the text file is encoded in UTF-8    
     df = pd.read_csv(os.path.join(lca_results_dir, filename), sep = ",")
-    
-    subj_counter = 0
-    for subj in range(len(df)):
-        df.at[subj_counter, 'subjcode'] = df.loc[subj_counter, 
-             'filename'][-7:-4] # Indexing to extract subject code
-        subj_counter += 1
-    
-    df.set_index('subjcode', inplace = True) # Set subject code as index
+    df.set_index('subjectcode', inplace = True) # Set subject code as index
     
     # Check whether all subject codes are unique
     if len(set(list(df.index))) != len(df):
@@ -33,14 +28,18 @@ def filter_df(df, measures):
     """A function that subsets only those columns (i.e., LCA measures) we are
     interested in, and in addition only includes writing samples with fifty
     words or more.""" 
-            
+    
     # Only use selected measures
     df = df.loc[: , measures]
     
     # Remove empty entries
-    #df = df[df.wordtokens >= 50]
+    #df = df[df.wordtokens != 0]
     
     return df
+
+
+def merge_df(exam, subject_info):
+    pass
 
 
 def diff_same_lang(df1, df2, measures):
@@ -75,6 +74,7 @@ def diff_other_lang(df1, df2, measures):
     print(diff_means)
 
 #pd.set_option('display.max_columns', None)
+#pd.set_option('display.max_rows', None)
 
 
 ### --------
@@ -88,38 +88,50 @@ def diff_other_lang(df1, df2, measures):
 parser = argparse.ArgumentParser()
 parser.add_argument("exam1", help = "The first exam in the comparison.")
 parser.add_argument("exam2", help = "The second exam in the comparison.")
-parser.add_argument("trunc", help = "Should the LCA be performed on samples \
+parser.add_argument("--truncation", help = "Should the LCA be performed on samples \
                     that were truncated at the same length?", choices = 
-                    ["trunc", "untrunc"])
+                    ["yes", "no"], default = "yes")
+parser.add_argument("--nationality", help = "For which nationality do you \
+                    want to perform the analysis?", choices = ["DU", "NL"], 
+                    default = "NL")
+
 args = parser.parse_args()
 name_exam1 = args.exam1
 name_exam2 = args.exam2
-trunc = args.trunc
+trunc = args.truncation
+natio = args.nationality
     
 # Define directories
 src_dir = os.path.dirname(os.path.abspath(__file__))
 data_dir = os.path.join(src_dir, '..', 'data')
 results_dir = os.path.join(src_dir, '..', 'results')
 
-if trunc == "trunc":
+if trunc == "yes":
     lca_results_dir = os.path.join(results_dir, 'lca_truncated')
-elif trunc == "untrunc":
+elif trunc == "no":
     lca_results_dir = os.path.join(results_dir, 'lca_untruncated')
 
 # Create dataframes with LCA results for the two exams
 exam1 = create_pandas_df(lca_results_dir, name_exam1)
 exam2 = create_pandas_df(lca_results_dir, name_exam2)
 
+# Establish the language each exam was written in
 lang1 = name_exam1[7:9]
 lang2 = name_exam2[7:9]
 
+# Read in subject info
+subject_info = prep.read_subject_info(data_dir)
+
 # Define required measures
 sel_measures = ['ld', 'ls2', 'vs2', 'ndwesz', 'cttr', 'svv1']
-all_measures = list(exam1.columns[1:]) # [1:] to exclude filename
+all_measures = list(exam1.columns)
 
 # Optionally, apply filters
 filtered_exam1 = filter_df(exam1, sel_measures)
 filtered_exam2 = filter_df(exam2, sel_measures)
+
+# Join dataframes
+joined_df = exam1.join(subject_info)
 
 # Compare the two exams
 print("Calculating:", name_exam2[:-4], "minus", name_exam1[:-4], "\n")
