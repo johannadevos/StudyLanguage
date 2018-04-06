9718a10ec49c752dcad52da1c7b45f2a35f584af
Johanna de Vos <johannadevos@gmail.com>
2018-04-06 11:30:48 +0200

    Introduced lca_min_sam in sample_size.py.

--- src/sample_size.py
+++ src/sample_size.py
@@ -19,33 +19,14 @@ def calculate_descriptives(files, data_dir, results_dir):
     output_dir = os.path.join(results_dir, 'length_descriptives')
     
     for exam in files:
-        exam = exam[:-4]
-            
-        if not "STAT_C" in exam:
-            print(exam)
-            
-            results_file = str(os.path.join(results_dir, 'lca_untruncated', 
-                                            exam)) + '.txt'
-            
-            results_df = pd.read_csv(results_file, sep = ',')
-            wordtokens = results_df['wordtokens']
-                    
-            print("Average length of writing samples:", statistics.mean
-                  (wordtokens))
-            print("Standard deviation of writing sample length:", statistics.
-                  stdev(wordtokens))
-            print("The longest sample is:", max(wordtokens), "words")
-            histogram(wordtokens, exam, output_dir=output_dir)
-        
-        elif "STAT_C" in exam:
-            questions = ['4a', '2aDec', '2aCaus']
-            
-            for question in questions:
-                print(exam, question)
+        if not "uncorrected" in exam:
+            exam = exam[:-4]
+                
+            if not "STAT_C" in exam:
+                print(exam)
                 
                 results_file = str(os.path.join(results_dir, 'lca_untruncated', 
-                                                exam)) + '_' + question \
-                                                + '.txt'
+                                                exam)) + '.txt'
                 
                 results_df = pd.read_csv(results_file, sep = ',')
                 wordtokens = results_df['wordtokens']
@@ -55,7 +36,27 @@ def calculate_descriptives(files, data_dir, results_dir):
                 print("Standard deviation of writing sample length:", 
                       statistics.stdev(wordtokens))
                 print("The longest sample is:", max(wordtokens), "words")
-                histogram(wordtokens, exam, question, output_dir=output_dir)
+                #histogram(wordtokens, exam, output_dir=output_dir)
+            
+            elif "STAT_C" in exam:
+                questions = ['4a', '2aDec', '2aCaus']
+                
+                for question in questions:
+                    print(exam, question)
+                    
+                    results_file = str(os.path.join(results_dir, 'lca_untruncated', 
+                                                    exam)) + '_' + question \
+                                                    + '.txt'
+                    
+                    results_df = pd.read_csv(results_file, sep = ',')
+                    wordtokens = results_df['wordtokens']
+                            
+                    print("Average length of writing samples:", statistics.mean
+                          (wordtokens))
+                    print("Standard deviation of writing sample length:", 
+                          statistics.stdev(wordtokens))
+                    print("The longest sample is:", max(wordtokens), "words")
+                    #histogram(wordtokens, exam, question, output_dir=output_dir)
     
 
 def histogram(variable, x_axis_title, question=None, output_dir=None):
@@ -108,7 +109,12 @@ def all_results(results_dir):
     return pd.concat(appended_data)
 
 
-def correlations(df, results_dir):
+def correlations(df, results_dir, lca_min_sam):
+    
+    # Only use samples that are longer than the specified cut-off point
+    df = df[df.wordtokens >= lca_min_sam]
+    
+    # Find columns that contain LCA measures
     index_ld = df.columns.get_loc('ld')
     lca_measures = df.columns[index_ld:]
     
@@ -130,7 +136,7 @@ def correlations(df, results_dir):
         
             for measure in lca_measures:
                 corr, sig = spearmanr(df['wordtokens'], df[measure])
-                #print(measure, "\tSpearman's r =", corr, "\tp = ", sig)
+                print(measure, "\tSpearman's r =", corr, "\tp = ", sig)
                 outfile.write("\t\t".join((measure, str(round(corr, 3)), str(
                         round(sig, 3)), "\n")))
                 
@@ -156,8 +162,11 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('language', help = "Choose the study language: 'EN' \
                         or 'NL'.", choices = ["EN", "NL"])
+    parser.add_argument('lca_min_sam', help = "Choose the minimal sample size \
+                    with which the data were preprocessed.")
     args = parser.parse_args()
     language = args.language
+    lca_min_sam = int(args.lca_min_sam)
     
     # Create directory to store results
     create_descr_dir(results_dir, 'length_descriptives')
@@ -168,7 +177,7 @@ def main():
 
     # Calculate correlations between length and measures
     big_df = all_results(results_dir)
-    correlations(big_df, results_dir)
+    correlations(big_df, results_dir, lca_min_sam)
     
 # Run code
 if __name__ == "__main__":
