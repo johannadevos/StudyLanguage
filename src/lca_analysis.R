588d323da3eeb03382e8b18cba8ed12f8c7e677e
Johanna de Vos <johannadevos@gmail.com>
2018-04-20 13:04:00 +0200

    Added an interaction to the MANOVA.

--- src/lca_analysis.R
+++ src/lca_analysis.R
@@ -214,13 +214,7 @@ man_data <- man_data[man_data$TrackNatio1 != "German_in_Dutch",]
 outcome <- cbind(man_data$LD, man_data$LS2, man_data$NDWESZ)
 
 # Run MANOVA
-manova <- manova(outcome ~ TrackNatio1 + Month, data = man_data)
+manova <- manova(outcome ~ TrackNatio1 + Month + TrackNatio1 * Month, data = man_data)
 summary(manova)
 summary.lm(manova)
 summary.aov(manova)
-
-
-
-
-
-
