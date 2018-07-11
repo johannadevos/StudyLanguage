4032f7c310d1ccaf80b4edd46dd7e4f3428572ff
Johanna de Vos <johannadevos@gmail.com>
2018-07-11 10:30:42 +0200

    Check assumptions for mixed-effects models of study success.

--- src/study_success.R
+++ src/study_success.R
@@ -451,6 +451,48 @@ weighted_lv <- update(weighted_null_lr, ~. + LV); summary(weighted_lv)
 anova(weighted_null_lr, weighted_lv)
 
 
+### Check assumptions
+
+# Choose the model you want to check the assumptions for
+model <- mean_gender
+
+## Is there a linear relationship between the dependent and independent variables?
+# The plot should not show any obvious pattern in the residuals
+plot(fitted(model), residuals(model))
+abline(h = 0)
+
+# A weak relationship is visible: smaller fitted values tend to have negative residuals,
+# larger fitted values seem to have positive residuals
+cor.test(fitted(model), residuals(model))
+summary(lm(residuals(model) ~ fitted(model)))
+
+## Absence of collinearity
+# Exam and Group are not correlated, because all groups took the same exams
+
+## Homoskedasticity
+# The standard deviations of the residuals should not depend on the x-value
+plot(fitted(model), residuals(model))
+abline(h = 0)
+# There doesn't seem to be heteroscedasticity - higher fitted values don't have smaller/larger residuals
+
+## Normality of residuals
+hist(residuals(model)) 
+qqnorm(residuals(model))
+# Almost perfectly normal
+
+## Absence of influential data points
+
+# Calculate Cook's distance and visualise outcomes
+lca_data$Cook <- cooks.distance.estex(influence(model, group = 'SubjectCode'))
+plot(lca_data$Cook, ylab = "Cook's distance")
+# Different guidelines. Either, Cook's distance should not be >1 or >0.85 (assumption met)
+# Or, it shouldn't be >4/n (assumption not met, but no real outliers)
+
+## Independence
+# Is taken care of by the random intercepts at the subject level
+
+
+
 ### --------------------------------------------
 ### Correlations between the dependent variables
 ### --------------------------------------------
