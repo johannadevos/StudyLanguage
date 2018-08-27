abf2d20d38329362260ef2cd615fd6a546f8b5e9
Johanna de Vos <johannadevos@gmail.com>
2018-08-27 13:40:13 +0200

    Removed parts of the script that are no longer used, and copied the mixed-effects models part to Question 5.

--- src/study_success.R
+++ src/study_success.R
@@ -102,6 +102,46 @@ no_dropout <- subject_info[subject_info$DropOut!="DuringYear1",]
 # Alpha according to Li & Ji (2005); cited in Nyholt (2004)
 alpha_success = 0.0170
 
+## Create long data frames needed to do mixed-effects modelling
+
+# Indices
+index1_grade <- which(colnames(no_dropout)=="Course1_Grade")
+index13_grade <- which(colnames(no_dropout)=="Course13_Grade")
+index1_worth <- which(colnames(no_dropout)=="Course1_EC_Worth")
+index13_worth <- which(colnames(no_dropout)=="Course13_EC_Worth")
+index1_ec <- which(colnames(no_dropout)=="Course1_EC_Obtained")
+index13_ec <- which(colnames(no_dropout)=="Course13_EC_Obtained")
+index1_weighted <- which(colnames(no_dropout)=="Course1_Weighted")
+index13_weighted <- which(colnames(no_dropout)=="Course13_Weighted")
+index1_passed <- which(colnames(no_dropout)=="Course1_Passed")
+index13_passed <- which(colnames(no_dropout)=="Course13_Passed")
+
+# Transform to long data format
+EC_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_ec:index13_ec), value.name = "EC_Obtained")
+colnames(EC_long)[colnames(EC_long)=="variable"] <- "Course"
+EC_long$Course <- as.factor(gsub("\\D", "", EC_long$Course))
+
+grades_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_grade:index13_grade), value.name = "Grade")
+colnames(grades_long)[colnames(grades_long)=="variable"] <- "Course"
+grades_long$Course <- as.factor(gsub("\\D", "", grades_long$Course))
+
+weighted_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_weighted:index13_weighted), value.name = "Weighted_Grade")
+colnames(weighted_long)[colnames(weighted_long)=="variable"] <- "Course"
+weighted_long$Course <- as.factor(gsub("\\D", "", weighted_long$Course))
+
+passed_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_passed:index13_passed), value.name = "Passed")
+colnames(passed_long)[colnames(passed_long)=="variable"] <- "Course"
+passed_long$Course <- as.factor(gsub("\\D", "", passed_long$Course))
+
+# Merge
+subject_long <- merge(EC_long, grades_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
+subject_long <- merge(subject_long, weighted_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
+subject_long <- merge(subject_long, passed_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
+
+# Remove data frames
+rm(EC_long, grades_long, weighted_long, passed_long)
+rm(list=ls(pattern="index"))
+
 
 ### TOTAL NUMBER OF OBTAINED ECs
 
@@ -161,6 +201,17 @@ wilcox_wide_ECs$SubjectCode <- NULL
 # Perform robust ANOVA with bootstrapping
 t1waybt(wilcox_wide_ECs, tr = 0, nboot = 10000)
 #med1way(wilcox_wide_ECs) # "WARNING: tied values detected. Estimate of standard error might be highly inaccurate, even with n large."
+
+# Mixed-effects models
+ec_null <- glmer(Passed ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, family = "binomial", control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); summary(ec_null)
+ec_group <- update(ec_null, ~. + Group); summary(ec_group)
+anova(ec_null, ec_group)
+
+# Binned residual plot (see Gelman & Hill, 2007)
+binnedplot(fitted(ec_group), resid(ec_group), cex.pts=1, col.int="black", xlab = "Predicted values")
+
+# Normality of residuals
+hist(residuals(ec_group))
 
 
 ### MEAN GRADE
@@ -232,6 +283,17 @@ wilcox_wide_mean$SubjectCode <- NULL
 t1waybt(wilcox_wide_mean, tr = 0, nboot = 10000)
 #med1way(wilcox_wide_mean) # "WARNING: tied values detected. Estimate of standard error might be highly inaccurate, even with n large."
 
+# Mixed-effects models
+mean_null <- lmer(Grade ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, REML = FALSE); summary(mean_null)
+mean_group <- update(mean_null, ~. + Group); summary(mean_group)
+anova(mean_null, mean_group)
+
+# Binned residual plot (see Gelman & Hill, 2007)
+binnedplot(fitted(mean_group), resid(mean_group), cex.pts=1, col.int="black", xlab = "Predicted values")
+
+# Normality of residuals
+hist(residuals(mean_group))
+
 
 ### WEIGHTED GRADE
 
@@ -288,6 +350,17 @@ wilcox_wide_weighted$SubjectCode <- NULL
 t1waybt(wilcox_wide_weighted, tr = 0, nboot = 10000)
 #med1way(wilcox_wide_weighted) # "WARNING: tied values detected. Estimate of standard error might be highly inaccurate, even with n large."
 
+# Mixed-effects models
+weighted_null <- lmer(Weighted_Grade ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, REML = FALSE); summary(weighted_null)
+weighted_group <- update(weighted_null, ~. + Group); summary(weighted_group)
+anova(weighted_null, weighted_group)
+
+# Binned residual plot (see Gelman & Hill, 2007)
+binnedplot(fitted(weighted_group), resid(weighted_group), cex.pts=1, col.int="black", xlab = "Predicted values")
+
+# Normality of residuals
+hist(residuals(weighted_group))
+
 
 ### Drop-out
 
@@ -343,6 +416,8 @@ chisq.test(bsa_no_dropout)
 ### Question 6: Does students' lexical richness in the study language predict their study success?
 ### ----------------------------------------------------------------------------------------------
 
+### Preprocessing
+
 # Define functions to do robust regression (taken Field, Miles & Field, 2012, p. 299)
 bootReg <- function (formula, data, i)
 {d <- data [i,] # i refers to a particular bootstrap sample
@@ -363,6 +438,14 @@ lca <- subject_info[subject_info$LCA==1,] # Incidentally, no-one dropped out dur
 lca$LD_Centered <- lca$LD - mean(lca$LD)
 lca$LS_Centered <- lca$LS - mean(lca$LS)
 lca$LV_Centered <- lca$LV - mean(lca$LV)
+
+# Create a long data frame with the lexical richness measures, for mixed-effects modelling
+lr_long <- subject_long
+lr_long$LD <- subject_info$LD[match(lr_long$SubjectCode, subject_info$SubjectCode)]
+lr_long$LS <- subject_info$LS[match(lr_long$SubjectCode, subject_info$SubjectCode)]
+lr_long$LV <- subject_info$LV[match(lr_long$SubjectCode, subject_info$SubjectCode)]
+lr_long <- lr_long[!is.na(lr_long$LD),]
+lr_long$LCA <- NULL
 
 
 ### TOTAL NUMBER OF OBTAINED ECs
@@ -531,12 +614,16 @@ boot.ci(lca_drop, type = "bca", conf = (1-alpha_success), index = 2) # Confidenc
 boot.ci(lca_drop, type = "bca", conf = (1-alpha_success), index = 3) # Confidence intervals for LS
 boot.ci(lca_drop, type = "bca", conf = (1-alpha_success), index = 4) # Confidence intervals for LV
 
+# Logistic regression
 lca_drop_glm <- glm(DropOutBinary ~ Group + LD_Centered + LS_Centered + LV_Centered, data = lca, family = "binomial"); summary(lca_drop_glm)
 
 # Check assumptions
 
 # Binned residual plot (see Gelman & Hill, 2007)
 binnedplot(fitted(lca_drop_glm), resid(lca_drop_glm), cex.pts=1, col.int="black", xlab = "Predicted values")
+
+# Histogram of residuals
+hist(residuals(lca_drop_glm))
 
 # Investigate outliers
 
@@ -612,16 +699,6 @@ which(lca_no_infl_cases$LargeResidual==TRUE & (lca_no_infl_cases$LargeCook==TRUE
 ### Inferential statistics per outcome variable (no-mixed effects models yet)
 ### -------------------------------------------------------------------------
 
-### OBTAINED ECS
-
-## Ordinary regression
-reg_group <- lm(EC_Obtained ~ Group, data = no_dropout)
-summary(reg_group)
-
-# Further predictors: gender, LD, LS, LV
-
-## Robust ANCOVA
-
 # Function to draw histograms per group for the lexical richness variables
 lr_hist <- function(dep_var){
   par(mfrow=c(2,2))
@@ -644,55 +721,12 @@ tapply(no_dropout$LD, no_dropout$Group, stat.desc)
 tapply(no_dropout$LS, no_dropout$Group, stat.desc)
 tapply(no_dropout$LV, no_dropout$Group, stat.desc)
 
-# Check assumptions for ANOVA
-leveneTest(no_dropout$LD, no_dropout$Group)
-
-# Conduct ANOVA
-ld_lm <- lm(LD ~ Group, data = no_dropout); summary(ld_lm)
-ls_lm <- lm(LS ~ Group, data = no_dropout); summary(ls_lm)
-lv_lm <- lm(LV ~ Group, data = no_dropout); summary(lv_lm)
-
-ld_aov <- aov(LD ~ Group, data = no_dropout, na.action = na.exclude); summary(ld_aov)
-ls_aov <- aov(LS ~ Group, data = no_dropout, na.action = na.exclude); summary(ls_aov)
-lv_aov <- aov(LV ~ Group, data = no_dropout, na.action = na.exclude); summary(lv_aov)
-
-# Conduct ANCOVA
-
-# NB: No robust ANCOVA for more than two groups seems to be available
-
-
-
-# Other analyses (to be continued)
-boot_gender <- boot(statistic = bootReg, formula = EC_Obtained ~ Group + Gender, data = no_dropout, R = 2000)
-
-bootResults2 <- boot(statistic = bootReg, formula = EC_Obtained ~ Group, data = lr, R = 2000)
-bootResults3 <- boot(statistic = bootReg, formula = EC_Obtained ~ Group + LD_Centered, data = lr, R = 2000)
-bootResults2$t0 # Intercept and slope coefficients
-boot.ci(bootResults3, type = "bca", conf = 0.991, index = 1) # Confidence intervals for intercept
-boot.ci(bootResults3, type = "bca", conf = 0.991, index = 2) # Confidence intervals for group: Dutch in English track
-boot.ci(bootResults3, type = "bca", index = 3) # Confidence intervals for group: German in Dutch track
-boot.ci(bootResults3, type = "bca", index = 4) # Confidence intervals for group: German in English track
-
 ## Lexical richness
 
 # Are the lexical richness measures normally distribued?
 hist(lca$LD) # Yes
 hist(lca$LS) # So-so (little bit of positive skew)
 hist(lca$LV) # Yes
-
-# Are the lexical richness measures correlated?
-rcorr(as.matrix(lca[,cbind("LD", "LS", "LV")]), type = "pearson")
-
-## Regression
-boot_lr <- boot(statistic = bootReg, formula = EC_Obtained ~ LV + LS + LD, data = lr, R = 2000)
-
-# Results
-boot_lr$t0 # Intercept and slope coefficients
-boot.ci(boot_lr, type = "bca", conf = 0.991, index = 1) # Confidence intervals for intercept
-boot.ci(boot_lr, type = "bca", conf = 0.991, index = 2) # Confidence intervals for group: Dutch in English track
-boot.ci(boot_lr, type = "bca", conf = 0.991, index = 3) # Confidence intervals for group: German in Dutch track
-boot.ci(boot_lr, type = "bca", conf = 0.991, index = 4) # Confidence intervals for group: German in English track
-
 
 ## Contrasts
 
@@ -728,208 +762,6 @@ contrasts(subject_info$Group)
 ## Gender as predictor
 dropout_model2 <- glm(DropOutBinary ~ Group + Gender, family = binomial (link = "logit"), data = subject_info)
 summary(dropout_model2)
-
-
-### ----------------------------------------------------------------------------------------------
-### Question 6: Does students' lexical richness in the study language predict their study success?
-### ----------------------------------------------------------------------------------------------
-
-# Dataset with just LCA
-lca <- subject_info[subject_info$LCA==1,] # Incidentally, no-one dropped out during year 1
-
-# Center the LCA measures
-lca$LD_Centered <- lca$LD - mean(lca$LD)
-lca$LS_Centered <- lca$LS - mean(lca$LS)
-lca$LV_Centered <- lca$LV - mean(lca$LV)
-
-# Merge subject info and LCA data
-lex <- merge(lca_data, subject_info, all.x = TRUE) # Get lca_data from lexical_richness script
-
-# Average lexical richness scores over the three exams
-lex$LD <- rowMeans(cbind(lex$ld_oct, lex$ld_feb, lex$ld_apr))
-lex$LS2 <- rowMeans(cbind(lex$ls2_oct, lex$ls2_feb, lex$ls2_apr))
-lex$NDWESZ <- rowMeans(cbind(lex$ndwesz_oct, lex$ndwesz_feb, lex$ndwesz_apr))
-
-# Models and model comparisons
-ld_first <- glm(DropOutBinary ~ Group + Gender + LD, family = binomial (link = "logit"), data = lex)
-summary(ld_first)
-
-ls_second <- glm(DropOutBinary ~ Group + Gender + LD + LS2, family = binomial (link = "logit"), data = lex)
-summary(ls_second)
-anova(ld_first, ls_second, test = "Chisq")
-
-ls_first <- glm(DropOutBinary ~ Group + Gender + LS2, family = binomial (link = "logit"), data = lex)
-summary(ls_first)
-
-ld_second <- glm(DropOutBinary ~ Group + Gender + LS2 + LD, family = binomial (link = "logit"), data = lex)
-summary(ld_second)
-anova(ls_first, ld_second, test = "Chisq")
-
-lv_model <- glm(DropOutBinary ~ Group + Gender + LD + LS2 + NDWESZ, family = binomial (link = "logit"), data = lex)
-summary(lv_model)
-anova(ls_model, lv_model, test = "Chisq")
-
-## To do: lsmeans / correct for multiple testing
-
-
-### ---------------------------
-### Extra: Mixed-effects models
-### ---------------------------
-
-### PREPROCESSING
-
-# Indices
-index1_grade <- which(colnames(no_dropout)=="Course1_Grade")
-index13_grade <- which(colnames(no_dropout)=="Course13_Grade")
-index1_worth <- which(colnames(no_dropout)=="Course1_EC_Worth")
-index13_worth <- which(colnames(no_dropout)=="Course13_EC_Worth")
-index1_ec <- which(colnames(no_dropout)=="Course1_EC_Obtained")
-index13_ec <- which(colnames(no_dropout)=="Course13_EC_Obtained")
-index1_weighted <- which(colnames(no_dropout)=="Course1_Weighted")
-index13_weighted <- which(colnames(no_dropout)=="Course13_Weighted")
-index1_passed <- which(colnames(no_dropout)=="Course1_Passed")
-index13_passed <- which(colnames(no_dropout)=="Course13_Passed")
-
-# Transform to long data format
-EC_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_ec:index13_ec), value.name = "EC_Obtained")
-colnames(EC_long)[colnames(EC_long)=="variable"] <- "Course"
-EC_long$Course <- as.factor(gsub("\\D", "", EC_long$Course))
-
-grades_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_grade:index13_grade), value.name = "Grade")
-colnames(grades_long)[colnames(grades_long)=="variable"] <- "Course"
-grades_long$Course <- as.factor(gsub("\\D", "", grades_long$Course))
-
-weighted_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_weighted:index13_weighted), value.name = "Weighted_Grade")
-colnames(weighted_long)[colnames(weighted_long)=="variable"] <- "Course"
-weighted_long$Course <- as.factor(gsub("\\D", "", weighted_long$Course))
-
-passed_long <- melt(no_dropout, id.vars=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "LCA"), measure.vars = c(index1_passed:index13_passed), value.name = "Passed")
-colnames(passed_long)[colnames(passed_long)=="variable"] <- "Course"
-passed_long$Course <- as.factor(gsub("\\D", "", passed_long$Course))
-
-# Merge
-subject_long <- merge(EC_long, grades_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
-subject_long <- merge(subject_long, weighted_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
-subject_long <- merge(subject_long, passed_long, by=c("SubjectCode", "Gender", "Track", "Nationality", "Group", "Course", "LCA"))
-
-# Remove dataframes
-rm(EC_long, grades_long, weighted_long, passed_long)
-rm(list=ls(pattern="index"))
-
-# Also create a long dataframe with the lexical richness measures, using a subset of all data
-lr_long <- subject_long
-lr_long$LD <- subject_info$LD[match(lr_long$SubjectCode, subject_info$SubjectCode)]
-lr_long$LS <- subject_info$LS[match(lr_long$SubjectCode, subject_info$SubjectCode)]
-lr_long$LV <- subject_info$LV[match(lr_long$SubjectCode, subject_info$SubjectCode)]
-lr_long <- lr_long[!is.na(lr_long$LD),]
-lr_long$LCA <- NULL
-
-
-### Models
-
-### TOTAL NUMBER OF OBTAINED ECs / PASSING COURSES
-
-# Investigate Group and Gender on the full dataset
-ec_null <- glmer(Passed ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, family = "binomial", control = glmerControl(optimizer = "bobyqa", optCtrl=list(maxfun=1e5))); summary(ec_null)
-ec_group <- update(ec_null, ~. + Group); summary(ec_group)
-ec_gender <- update(ec_group, ~. + Gender); summary(ec_gender)
-anova(ec_null, ec_group, ec_gender)
-
-# Are the outcomes different for students who completed all three exams?
-ec_lca <- update(ec_group, ~. + LCA); summary(ec_lca)
-anova(ec_group, ec_lca)
-
-# Also use lexical richness as predictor, on the subset of the data for which these measures are available
-ec_null_lr <- glmer(Passed ~ 1 + (1|SubjectCode) + (1|Course), data = lr_long, REML = FALSE); summary(ec_null_lr)
-
-ec_ld <- update(ec_null_lr, ~. + LD); summary(ec_ld)
-anova(ec_null_lr, ec_ld) # Not significant
-
-ec_ls <- update(ec_null_lr, ~. + LS); summary(ec_ls)
-anova(ec_null_lr, ec_ls) # Not significant
-
-ec_lv <- update(ec_null_lr, ~. + LV); summary(ec_lv)
-anova(ec_null_lr, ec_lv) # Not significant
-
-
-### MEAN GRADE
-
-# Investigate Group and Gender on the full dataset
-mean_null <- lmer(Grade ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, REML = FALSE); summary(mean_null)
-mean_group <- update(mean_null, ~. + Group); summary(mean_group)
-mean_gender <- update(mean_group, ~. + Gender); summary(mean_gender)
-anova(mean_null, mean_group, mean_gender)
-
-# Also use lexical richness as predictor, on the subset of the data for which these measures are available
-mean_null_lr <- lmer(Grade ~ 1 + (1|SubjectCode) + (1|Course), data = lr_long, REML = FALSE); summary(mean_null_lr)
-
-mean_ld <- update(mean_null_lr, ~. + LD); summary(mean_ld)
-anova(mean_null_lr, mean_ld)
-
-mean_ls <- update(mean_null_lr, ~. + LS); summary(mean_ls)
-anova(mean_null_lr, mean_ls)
-
-mean_lv <- update(mean_null_lr, ~. + LV); summary(mean_lv)
-anova(mean_null_lr, mean_lv)
-
-
-### WEIGHTED GRADE
-
-# Investigate Group and Gender on the full dataset
-weighted_null <- lmer(Weighted_Grade ~ 1 + (1|SubjectCode) + (1|Course), data = subject_long, REML = FALSE); summary(weighted_null)
-weighted_group <- update(weighted_null, ~. + Group); summary(weighted_group)
-weighted_gender <- update(weighted_group, ~. + Gender); summary(weighted_gender)
-anova(weighted_null, weighted_group, weighted_gender)
-
-# Also use lexical richness as predictor, on the subset of the data for which these measures are available
-weighted_null_lr <- lmer(Weighted_Grade ~ 1 + (1|SubjectCode) + (1|Course), data = lr_long, REML = FALSE); summary(weighted_null_lr)
-weighted_ld <- update(weighted_null_lr, ~. + LD); summary(weighted_ld)
-anova(weighted_null_lr, weighted_ld)
-
-weighted_ls <- update(weighted_null_lr, ~. + LS); summary(weighted_ls)
-anova(weighted_null_lr, weighted_ls)
-
-weighted_lv <- update(weighted_null_lr, ~. + LV); summary(weighted_lv)
-anova(weighted_null_lr, weighted_lv)
-
-
-### Check assumptions
-
-# Choose the model you want to check the assumptions for
-model <- ec_group
-
-## Is there a linear relationship between the dependent and independent variables?
-# The plot should not show any obvious pattern in the residuals
-
-## Homoskedasticity
-# The standard deviations of the residuals should not depend on the x-value
-
-# Residual plot
-plot(fitted(model), residuals(model))
-abline(h = c(0, sd(residuals(model)), -sd(residuals(model))))
-
-# Binned residual plot (see Gelman & Hill, 2007)
-binnedplot(fitted(model), resid(model), cex.pts=1, col.int="black", xlab = "Predicted values")
-
-# Correlation test (in case a pattern is visible)
-cor.test(fitted(model), residuals(model))
-
-## Absence of collinearity
-
-## Normality of residuals
-hist(residuals(model)) 
-qqnorm(residuals(model))
-
-## Absence of influential data points
-
-# Calculate Cook's distance and visualise outcomes
-subject_long$Cook <- cooks.distance.estex(influence(model, group = 'SubjectCode'))
-plot(subject_long$Cook, ylab = "Cook's distance")
-# Different guidelines. Either, Cook's distance should not be >1 or >0.85 (assumption met)
-# Or, it shouldn't be >4/n (assumption not met, but no real outliers)
-
-## Independence
-# Is taken care of by the random intercepts at the subject level
 
 
 
